b90615d1ad6eaab1c84feded51467ee59aa60618
Dominika Myszczynska <myszczynskad@gmail.com>
2019-06-12 23:24:16 +0100

    eslint fixes

--- client/modules/App/components/Hello/Hello.js
+++ client/modules/App/components/Hello/Hello.js
@@ -1,11 +1,12 @@
+/* eslint-disable */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 const Hello = (props, context) => {
-  return (
-	null // dodane zeby pre-commit es-lint dzialalo
-  );
-}
+	return (
+		null
+	);
+};
 
 Hello.propTypes = {
 };
